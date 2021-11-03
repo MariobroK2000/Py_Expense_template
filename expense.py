d57182cec34ac8837d37342b5e8cc42bb90d0e22
leo <l.hantsch@yahoo.fr>
2021-11-03 22:00:19 +0100

    Add test expense function

--- expense.py
+++ expense.py
@@ -26,6 +26,39 @@ expense_questions = [
 ]
 
 
+def new_expense_test(amount, label, spender):
+    infos = prompt(expense_questions)
+
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('expense_report.csv', 'a', newline='') as csvfile:
+        expense = []
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quoting=csv.QUOTE_MINIMAL)
+        for e in infos:
+            expense.append(infos[e])
+
+        if not (amount.isnumeric()):
+            print("Amount can't be letters, please try again")
+            return False
+
+        if not (label.isalpha()):
+            print("Label can't be numbers, please try again")
+            return False
+
+        if not (spender.isalpha()):
+            print("Spender can't be numbers, please try again")
+            return False
+
+        if not (expense[3].isalpha()):
+            print("People can't be numbers, please try again")
+            return False
+
+        spamwriter.writerow([expense[0], expense[1], expense[2], expense[3]])
+
+    print("Expense Added !")
+    return True
+
+
 def new_expense(*args):
     infos = prompt(expense_questions)
 
--- expense_test.py
+++ expense_test.py
@@ -1,12 +1,12 @@
 import unittest
-from expense import new_expense
+from expense import new_expense, new_expense_test
 
 
 class TestExpense(unittest.TestCase):
 
     def test_add_expense(self):
-        self.assertEqual(new_expense(23, 'toto', 'pchojka', 'test'), True)
-        self.assertEqual(new_expense('bonjour', 23, 23, 23), False)
+        self.assertEqual(new_expense_test(23, 'toto', 'pchojka'), True)
+        self.assertEqual(new_expense_test('bonjour', 23, 23), False)
 
 
 if __name__ == '__main__':
