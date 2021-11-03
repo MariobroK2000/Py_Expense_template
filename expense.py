7810702eeba92b3d52de7f6dce5fee6301c5a5b0
leo <l.hantsch@yahoo.fr>
2021-11-03 20:22:41 +0100

    Implementation of the first feature

--- expense.py
+++ expense.py
@@ -1,30 +1,38 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
-        "type":"input",
-        "name":"amount",
-        "message":"New Expense - Amount: ",
+        "type": "input",
+        "name": "amount",
+        "message": "New Expense - Amount: ",
     },
     {
-        "type":"input",
-        "name":"label",
-        "message":"New Expense - Label: ",
+        "type": "input",
+        "name": "label",
+        "message": "New Expense - Label: ",
     },
     {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
+        "type": "input",
+        "name": "spender",
+        "message": "New Expense - Spender: ",
     },
 
 ]
 
 
-
 def new_expense(*args):
     infos = prompt(expense_questions)
+
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('expense_report.csv', 'a', newline='') as csvfile:
+        expense = []
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quoting=csv.QUOTE_MINIMAL)
+        for e in infos:
+            expense.append(infos[e])
+
+        spamwriter.writerow([expense[0], expense[1], expense[2]])
+
     print("Expense Added !")
     return True
-
-
